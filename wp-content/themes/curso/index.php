958719a6932432ca2b6040927f6718503ee3a1f2
maiCavalheiro <mairadanielec@gmail.com>
2018-07-17 19:08:27 -0400

    aula 24
    aula 24

--- wp-content/themes/curso/index.php
+++ wp-content/themes/curso/index.php
@@ -22,8 +22,7 @@
                         // enquanto houver posts, chama o post
                         while (have_posts()): the_post();
                       ?>
-
-                      <?php get_template_part('template-parts/content'); ?>
+                      <?php get_template_part('template-parts/content', get_post_format()); ?>
 
                       <?php
                     endwhile;
