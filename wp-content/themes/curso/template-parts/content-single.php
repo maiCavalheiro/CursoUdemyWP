ef4b286a45e267dc4a81966f447f1b8184df7c32
Maira Cavalheiro <mairadanielec@gmail.com>
2018-07-24 17:02:55 -0400

    aula 33

---
+++ wp-content/themes/curso/template-parts/content-single.php
@@ -0,0 +1,14 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header>
+		<h1><?php the_title(); ?></h1>
+		<div class="meta-info">
+			<p>Published in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+			<p>Categories: <?php the_category(', '); ?></p>
+			<p><?php the_tags('Tags: ',', '); ?></p>
+		</div>
+	</header>
+	<div class="content">
+		<?php the_content(); ?>
+	</div>
+	
+</article>
