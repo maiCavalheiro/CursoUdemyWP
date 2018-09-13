26b85fab744892d248998d9704d6cccded4849b2
Maira Cavalheiro <mairadanielec@gmail.com>
2018-09-13 17:06:47 -0400

    Slider options

--- wp-content/themes/curso/inc/customizer.php
+++ wp-content/themes/curso/inc/customizer.php
@@ -69,5 +69,68 @@ function wpcurso_customizer( $wp_customize){
     )
   );
 
+    //Slide
+  $wp_customize ->add_section(
+    'sec_slide', array(
+      'title' =>'Slide',
+      'description' => 'Slide Section'
+    )
+  );
+
+    //Design 
+  $wp_customize -> add_setting(
+    'set_slide_option', array(
+      'type' => 'theme_mod',
+      'default' => '1',
+      'sanitize_callback' => 'wpcurso_sanitize_select'
+    )
+  );
+
+   $wp_customize -> add_control(
+    'set_slide_option', array(
+      'label' => 'Choose your design type here',
+      'description' => 'Choose your design type',
+      'section' => 'sec_slide',
+      'type' => 'select',
+      'choices' => array(
+        '1' => 'Design type 1',
+        '2' => 'Design type 2',
+        '3' => 'Design type 3',
+        '4' => 'Design type 4',
+      )
+    )
+  );
+
+   //limit
+   $wp_customize -> add_setting(
+    'set_slide_limit', array(
+      'type' => 'theme_mod',
+      'default' => '5',
+      'sanitize_callback' => 'absint'
+    )
+  );
+
+   $wp_customize -> add_control(
+    'set_slide_limit', array(
+      'label' => 'Number of posts to display',
+      'description' => 'Choose the number of posts to be displayed',
+      'section' => 'sec_slide',
+      'type' => 'number'
+    )
+  );
+
 }
 add_action('customize_register', 'wpcurso_customizer' );
+
+   function wpcurso_sanitize_select( $input, $setting ){
+         
+            //input must be a slug: lowercase alphanumeric characters, dashes and underscores are allowed only
+            $input = sanitize_key($input);
+ 
+            //get the list of possible select options 
+            $choices = $setting->manager->get_control( $setting->id )->choices;
+                             
+            //return input if valid or return default option
+            return ( array_key_exists( $input, $choices ) ? $input : $setting->default );                
+             
+        }
